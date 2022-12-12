4265805fba5b382494ccc08732b5ca68937ef414
David Hanley <dhanley@groupon.com>
2022-12-11 21:41:39 -0700

    got day 2 working

--- 2/s.ml
+++ 2/s.ml
@@ -3,6 +3,11 @@
 
 type shapes = Rock | Paper | Scissors
 
+let shape_to_string = function
+  | Rock -> "rock"
+  | Paper -> "paper"
+  | Scissors -> "scissors"
+
 let char_to_shape m = match m with
   | 'A' | 'X' -> Rock
   | 'B' | 'Y' -> Paper
@@ -18,21 +23,24 @@ let my_value shape = match shape with
    | Paper -> 2
    | Scissors -> 3
 
-let win = 6
-let tie = 3
-let loss = 0
+type results = Win | Loss | Draw
 
-let first_match_result (his,mine) = my_value mine + (match (his,mine) with
-  | (Rock,Paper) -> win
-  | (Rock,Scissors) -> loss
-  | (Paper,Scissors) -> win
-  | (Paper,Rock) -> loss
-  | (Scissors,Rock) -> win
-  | (Scissors,Paper) -> loss
-  | _ -> tie)
+let result_value = function
+ | Win -> 6
+ | Draw -> 3
+ | Loss -> 0
 
-let line_to_round_1 (h,m) =
-  (char_to_shape h, char_to_shape m)
+let first_match_result (his,mine) = my_value mine + (result_value (match (his,mine) with
+  | (Rock,Paper) -> Win
+  | (Rock,Scissors) -> Loss
+  | (Paper,Scissors) -> Win
+  | (Paper,Rock) -> Loss
+  | (Scissors,Rock) -> Win
+  | (Scissors,Paper) -> Loss
+  | _ -> Draw))
+
+let line_to_round_1 (h, m) =
+(char_to_shape h, char_to_shape m)
 
 let file_to_strings fn =
   let inf = open_in fn in
@@ -60,10 +68,35 @@ let round_1 () =
   let shape_shape_pairs = List.map line_to_round_1 pairs in
   Printf.printf "Round 1: %d\n" (sum_list (List.map first_match_result shape_shape_pairs))
 
+let char_to_result c = match c with
+  | 'X' -> Loss
+  | 'Y' -> Draw
+  | 'Z' -> Win
 
+let pair_to_shape_result (h,m) =
+  (char_to_shape h, char_to_result m)
+
+let pairs_shapes = List.map pair_to_shape_result pairs
+
+let score_round_2 (his_shape, result) =
+  let my_shape = match (his_shape, result) with
+   | (Rock, Loss) -> Scissors
+   | (Rock, Win)  -> Paper
+   | (Paper, Win) -> Scissors
+   | (Paper, Loss) -> Rock
+   | (Scissors, Win) -> Rock
+   | (Scissors, Loss) -> Paper
+   | _ -> his_shape in
+  let shapeScore =  (my_value my_shape) in
+   (result_value result) + shapeScore
+
+let round_2 () =
+  assert ((score_round_2 (Rock, Draw)) = 4);
+      Printf.printf "Round 1: %d\n" (sum_list (List.map score_round_2 pairs_shapes))
 
 let () =
-  round_1 ()
+  round_1 ();
+  round_2 ()
 
 
 
